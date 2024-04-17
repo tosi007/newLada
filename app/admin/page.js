d47c59227e51356bb9b55b4edabf3ea14cd930e8
tosi007 <85728191+tosi007@users.noreply.github.com>
2024-04-17 06:28:49 -0700

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -11,7 +11,7 @@ const Admin =() => {
           const [password, setPassword] = useState(""); // Use setPassword instead of setpassword
            const router = useRouter()
             const login = async () => {
-          const res = await fetch("http://localhost:5000/api/auth/login", {
+          const res = await fetch("https://apis-rouge.vercel.app/api/auth/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
