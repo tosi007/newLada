73b947d66902513129d1f7c980f10300e1321543
tosi007 <85728191+tosi007@users.noreply.github.com>
2024-05-04 11:40:49 -0700

    Update page.js

--- app/top/page.js
+++ app/top/page.js
@@ -9,6 +9,103 @@ const Ass = () => {
   const [data, setData] = useState("");
 
   const getData = async () => {
+    const res = await fetch("http://localhost:5000/api/product/gett", {
+      method: "GET",
+    });
+    const json = await res.json();
+    console.log(json);
+    setData(json);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className="text-center py-6">
+        <h1 style={{ fontFamily: "Dosis", fontWeight: 400, fontSize: "40px" }}>
+          Top Sellers
+        </h1>
+      </div>
+      <div className="grid justify-center items-center mx-12 space-x-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {Array.isArray(data) ? (
+          data.map((item) => (
+            <div
+              key={item._id}
+              className="w-60 h-[27rem] border-slate-200 border m-2"
+            >
+              <Image
+                className="w-full h-[50%]"
+                alt="Mountains"
+                src={item.Pic}
+                width={200}
+                height={200}
+                style={{
+                  objectPosition: "center",
+                  objectFit: "cover", // cover, contain, none
+                }}
+              />
+              <div className="h-6 py-8 ml-2 text-lg truncate">{item.name}</div>
+              <div
+                style={{
+                  fontFamily: "Dosis",
+                  fontWeight: 200,
+                  fontSize: "20px",
+                }}
+                className="pt-11 ml-2"
+              >
+                PKR, {item.Price}
+              </div>
+              <div>
+                <Link href={`/fproduct/${item._id}`}>
+                  <button
+                    style={{
+                      fontFamily: "Dosis",
+                      fontWeight: 200,
+                      fontSize: "20px",
+                    }}
+                    className="border hover:bg-black hover:text-white px-10 py-1 mb-2 ml-2"
+                  >
+                    View
+                  </button>
+                </Link>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Loading ....</p>
+        )}
+        <div className="text-center">
+          <Link href="/shop">
+            <button
+              style={{ fontFamily: "Dosis", fontWeight: 600, fontSize: "14px" }}
+              className="border bg-black text-white px-6 py-2"
+            >
+              SHOP ALL PRODUCTS
+            </button>
+          </Link>
+        </div>
+        <Fass />
+      </div>
+    </>
+  );
+};
+
+export default Ass;
+
+{
+  /*
+    "use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import Fass from "../asss/page";
+import CardSlider from "../asss/page";
+import Link from "next/link";
+
+const Ass = () => {
+  const [data, setData] = useState("");
+
+  const getData = async () => {
     const res = await fetch("https://apis-rouge.vercel.app/api/product/gett", {
       method: "GET",
     });
@@ -88,3 +185,8 @@ const Ass = () => {
 };
 
 export default Ass;
+
+   
+   
+    */
+}
