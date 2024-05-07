d6d9c200f346ec45db173070a9490914d7f917d6
tosi007 <85728191+tosi007@users.noreply.github.com>
2024-05-07 18:02:09 +0500

    Update page.js

--- app/new/page.js
+++ app/new/page.js
@@ -69,8 +69,8 @@ const Navs = () => {
                 }`}
               >
                 <svg
-                  width="70"
-                  height="40"
+                  width="90"
+                  height="60"
                   viewBox="0 0 200 124.85486590026754"
                   className="looka-1j8o68f"
                 >
