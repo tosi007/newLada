3accd86dfc1ec41c5a3483285527201779babd02
tosi007 <85728191+tosi007@users.noreply.github.com>
2024-12-21 23:55:22 -0800

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -47,7 +47,7 @@ const FeedBack = React.forwardRef((props, ref) => {
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M16 12l4-4m0 0l-4-4m4 4H8m6 4l-4 4m0 0l4-4m-4 4H8" />
       </svg>
-      <span class="text-gray-700 text-lg">ladajewellers1@gmail.com</span>
+      <span class="text-gray-700 text-lg">info@ladajewellers.com</span>
     </div>
   </div>
 </div>
